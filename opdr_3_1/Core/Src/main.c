4e76d805e5bfbed3c9fb72f07d04bcd602e6a011
Sem van der Hoeven <semmer99@gmail.com>
2022-09-12 14:19:44 +0200

    finish 3.1b

--- opdr_3_1/Core/Src/main.c
+++ opdr_3_1/Core/Src/main.c
@@ -5,8 +5,23 @@
 #define GPIOD_MODER *(volatile uint32_t*)(GPIOD_BASE + 0x00)
 #define GPIOD_ODR *(volatile uint32_t*)(GPIOD_BASE + 0x14) // RM0383 8.4.6
 
+#define STK_BASE 0xE000E010
+#define STK_CTRL *(volatile uint32_t*)(STK_BASE + 0x00) // control register
+#define STK_LOAD *(volatile uint32_t*)(STK_BASE + 0x04) // load reload register
+
 int main(void)
 {
+
+	// put count value in reload register
+	if (STK_CTRL & (1 << 2)) {
+		STK_LOAD = 8000000; // AHB is 16 MHz, so 0.5s is 8000000
+	} else {
+		STK_LOAD = 1000000; // AHB/8 is 2 MHz, so 0.5s is 1000000
+	}
+
+	STK_CTRL |= 1; // enable systick timer
+
+
 	// GPIO Port D Clock Enable
 	RCC_AHB1ENR_BIT_GPIODEN = 1;
 	// GPIO Port D Pin 15 down to 12 Push/Pull Output
@@ -17,7 +32,7 @@ int main(void)
 	while (1)
 	{
 		// Wait a moment
-		for (volatile int32_t i = 0; i < 1000000; i++);
+		while (( STK_CTRL & (1 << 16) ) == 0) ;
 		// Flip all LEDs
 		GPIOD_ODR ^= 0xF000;
 	}
